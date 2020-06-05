f8c48a3285031a657ce80f151b47dea25fedb790
Keariph <ryhlinskaya@bk.ru>
2020-06-05 20:16:00 +0700

    Creating a database connection

--- src/main/java/ru/aryhlinskaya/Diplom/Application.java
+++ src/main/java/ru/aryhlinskaya/Diplom/Application.java
@@ -1,7 +1,11 @@
 package ru.aryhlinskaya.Diplom;
 
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.boot.SpringApplication;
+import ru.aryhlinskaya.Diplom.config.DBConfig;
+
 public class Application {
     public static void main(String args[]){
-
+        SpringApplication.run(Application.class, args);
     }
 }
--- src/main/java/ru/aryhlinskaya/Diplom/config/DBConfig.java
+++ src/main/java/ru/aryhlinskaya/Diplom/config/DBConfig.java
@@ -1,4 +1,14 @@
 package ru.aryhlinskaya.Diplom.config;
 
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.*;
+import org.bson.Document;
+
 public class DBConfig {
+    public String connect(){
+        MongoClient client = MongoClients.create();
+        MongoDatabase database = client.getDatabase("ElectronicJournal");
+        return "hello";
+    }
 }
